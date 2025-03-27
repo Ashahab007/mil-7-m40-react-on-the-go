42db3173fd1e1e8972466030d76af10ccdacef6e
Ammar Shahab <ammar.qa@generalpharma.com>
2025-03-27 13:45:48 +0600

    third

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,17 +1,62 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Country from "../Country/Country";
 import "./Countries.css";
 
 const Countries = ({ responseCountries }) => {
+  // ? 1.1 Make and update state on parent of <Countries> component
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
+  // ? 1.2 Create an eventhandler and passed as props of <Country></Country> component
+
+  const handleVisitedCountries = (country) => {
+    //? 1.5 receive the country from  handleVisitedCountries(country)
+    // console.log("receive the country", country.name.common);
+    //? 1.6 এখানে সাধারণত push করে array তে add করা যেত কিন্তু react এ primitive and non-primitive data type এর ক্ষেত্রে state update different ভাবে কাজ করতে হয়। primitive data type change এর সাথে সাথে update হয়ে যায় কিন্তু non-primitive data type এর ক্ষেত্রে নুতুন reference এ  নিয়ে  সেটাকে রাখতে হয় যাতে দুইটা দুই reference এর আলাদা জিনিস। তা করার জন্য একটা নতুন reference এ  copy করে রেখে নতুন update add করতে হয়। নিম্নে তাই করা হল।
+    const newVisitedCountries = [...visitedCountries, country];
+    console.log(newVisitedCountries[0].name.common);
+
+    //? 1.7 set the newVisitedCountries to the visitedCountries
+    setVisitedCountries(newVisitedCountries);
+  };
+
+  // ? 2.1
+  const [visitedFlags, setVisitedFlags] = useState([]);
+  // ? 2.2
+  const handleAddFlags = (flags) => {
+    console.log("Flag is added", flags);
+    // ? 2.6
+    const newVisitedFlags = [...visitedFlags, flags];
+    setVisitedFlags(newVisitedFlags);
+    console.log("Flag is added", visitedFlags);
+  };
+
   const countries = use(responseCountries);
   console.log(countries);
 
   return (
     <div>
       <h2>Travelling Countries: {countries.length}</h2>
+      <h2>Country Travelled: {visitedCountries.length} </h2>
+      <div className="visited-flags">
+        {visitedFlags.map((flag, index) => (
+          <img src={flag} key={index}></img> //এখানে যেহেতু id নাই তাই index দিয়ে key এর error টা solve করা হয়েছে।
+        ))}
+      </div>
+      <ol>
+        {/* 1.8 Loop over the visitedCountries  */}
+        {visitedCountries.map((country) => (
+          <li>{country.name.common}</li>
+        ))}
+      </ol>
       <div className="countries">
         {countries.map((country) => (
-          <Country key={country.cca2} country={country}></Country>
+          <Country
+            key={country.cca2}
+            country={country}
+            handleVisitedCountries={handleVisitedCountries}
+            // 2.3
+            handleAddFlags={handleAddFlags}
+          ></Country>
         ))}
       </div>
     </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,18 +1,49 @@
-import React from "react";
+// ? Challange 1: Lift up the state to handle child component event i.e যে country তে click করবো তা ui এ দেখাব। যা parent i.e <Countries> থেকে কাজ করতে হয়।
+
+// ? Challenge 2: Lift up the state to handle child component event i.e এবার আলাদা button এর মাধ্যমে যে flag টা add করবো টা ui তে দেখাব।
+
+import React, { useState } from "react";
 import "./Country.css";
 
-const Country = ({ country }) => {
-  console.log(country);
+const Country = ({ country, handleVisitedCountries, handleAddFlags }) => {
+  // ? 1.3 receive the props handleVisitedCountries
+  // ? 2.4 receive the props handleAddFlags
+  // console.log(country);
+
+  const [visited, setVisited] = useState(false);
 
   const handleOnClick = () => {
-    console.log("click");
+    // console.log("click");
+    if (visited === true) {
+      setVisited(false);
+    } else {
+      setVisited(true);
+
+      //? 1.4 handleVisitedCountries is an eventHandler so called in another eventHandler and passing the country as a parameter.
+      handleVisitedCountries(country);
+      console.log("handleVisitedCountries working");
+    }
+    //setVisited(!visited);//It's the shortcut of if else condition in one line
   };
+
   return (
-    <div className="country">
+    <div className={`country ${visited && "country-visited"}`}>
+      {/* এখানে className টা dynamically design করা হয়েছে। */}
       <h4>Name: {country.name.common}</h4>
       <img src={country.flags.png} alt="" srcset="" />
       <h5>Population: {country.population}</h5>
-      <button onClick={handleOnClick}>Not Visited</button>
+      <h5>Independent: {country.independent ? "Free" : "Not Free"}</h5>
+      <button onClick={handleOnClick}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
+      {/* 2.5 */}
+      <button
+        onClick={() => {
+          handleAddFlags(country.flags.png);
+        }}
+      >
+        Add Flags
+      </button>
     </div>
   );
 };
